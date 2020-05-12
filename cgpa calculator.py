298226db5a38a2cd48fb4d7ea793e881a7dde3bd
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2020-05-12 22:41:20 +0100

    CGPA Calculator
    This is a CGPA calculator I built to compute the CGPA of an user

---
+++ cgpa calculator.py
@@ -0,0 +1,36 @@
+number_of_courses = 0
+print ('Welcome to this cgpa calculator')
+print ('''Type 'done' to end this calculator''')
+while True :
+    grade = (input('Please input your score for the course: '))
+    unit= (input('Please input the units of this course '))
+    if grade == 'done' or unit == 'done' :
+        break
+    try :
+        grade = int(grade)
+        unit= int(unit)
+    except :
+        print ('You have to input a number')
+        continue
+    if grade > 100 :
+        print('''Your grade can't be greater than 100''')
+        continue
+    elif grade >= 70 :
+        points = 7
+    elif grade >= 65 :
+        points = 6
+    elif grade >= 60 :
+        points = 5
+    elif grade >= 55 :
+        points = 4
+    elif grade >= 50 :
+        points = 3
+    elif grade >= 45 :
+        points = 2
+    elif grade >= 40 :
+        points = 1
+    else :
+        points = 0
+    number_of_courses = number_of_courses + 1
+    GPA = (points*unit)/ number_of_courses
+print ('Your GPA is',GPA)
