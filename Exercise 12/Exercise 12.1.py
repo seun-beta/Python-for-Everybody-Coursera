d31d77fd4912a1720fae00887b01bf13ea09b8c9
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2020-07-23 14:22:00 +0100

    Update Exercise 12.1.py

--- Exercise 12/Exercise 12.1.py
+++ Exercise 12/Exercise 12.1.py
@@ -1,1 +1,26 @@
+import socket 
+
+url = input('Input the url: ')
+port = int(input ('Input port: '))
+
+split_url = url.split('/')
+try:
+    host = split_url[2]
+except:
+    print ('The url is not correct')
+    quit()
+
+
+mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+    mysock.connect((host,port))
+except:
+    print ('Cannot use host')
+cmd = 'GET ' + url + ' HTTP/1.0 \r\n\r\n'
+command = cmd.encode()
+print (command)
+try:
+    mysock.send(command)
+except:
+    print ('The url is faulty')
 
